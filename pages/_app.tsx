73223cc5d9a9cb6e22523ceca105938b8df58103
Steven Kwan <stevent.kwan@gmail.com>
2023-07-25 22:43:21 -0700

    cleared boilerplate code; app ready to begin development

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// Import React-Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import global styles (can only be used in _app.tsx file)
+import '@/styles/globals.css';
+
+// Import styles for React-Bootstrap Container component for pages
+import styles from '@/styles/App.module.css';
+
+
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+
+import { Container } from 'react-bootstrap';
+
+// Save reference to Inter font to wrap App
+const inter = Inter({ subsets: ['latin'] });
+
+// The App component wraps our entire application
+// Elements that wrap Component are displayed on every page
+// Component pages are passed in with props and rendered
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Head>
+        <title>ExamShark</title>
+        <meta name="description" content="An app to create custom exams" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
+    </div>
+  )
 }
---
+++ pages/search.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const SearchExams = () => {
+  return ( 
+    <main>
+      <h1>Search Exams</h1>
+      <Link href='/'>back to home page</Link>
+    </main>
+   );
+}
+ 
+export default SearchExams;
