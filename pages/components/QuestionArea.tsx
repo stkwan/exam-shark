6381c3e7fa976fc63be1a75b26cf22a7c8314243
Steven Kwan <stevent.kwan@gmail.com>
2023-08-10 23:40:47 -0700

    created modal with form to allow user to save questions to exam

--- pages/components/QuestionArea.tsx
+++ pages/components/QuestionArea.tsx
@@ -19,18 +19,22 @@ export default function QuestionArea ( { questions }: QuestionResponse ) {
     event.preventDefault();
     const { correct } = choice;
     const target = event.target as HTMLParagraphElement;
-    if (target.tagName === 'P') {
-      target.firstElementChild!.classList.toggle(styles.correct);
-    } else if (target.tagName === 'IMG') {
-      target.parentElement!.classList.toggle(styles.correct);
-    } else if (target.tagName === 'SPAN') {
-      target.classList.toggle(styles.correct);
+
+    switch (target.tagName) {
+      case 'P':
+        target.firstElementChild!.classList.toggle(styles.correct);
+        break;
+      case 'IMG':
+        target.parentElement!.classList.toggle(styles.correct);
+        break;
+      case 'SPAN':
+        target.classList.toggle(styles.correct);
+        break;
     }
   }
 
   return (
     <div>
-
       <Accordion>
       {questions.map(question => {
         return (
@@ -56,51 +60,9 @@ export default function QuestionArea ( { questions }: QuestionResponse ) {
               })}
             </Accordion.Body>
           </Accordion.Item>
-
         );
       })}
-
-      {/*
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-
-      {questions.map(question => {
-        return (
-            <div 
-              key={`${question.id}`}
-              className={styles.sideQuestion}
-            >
-              <span className={styles.bigNumber}>{question.number}</span>
-            {question.prompt}
-              <button type="button" className="btn btn-primary" onClick={handleClick}>View</button>
-            </div>
-        )
-      })}
-    */}
-    </Accordion>
+      </Accordion>
     </div>
   );
 }
--- pages/exam/[id].tsx
+++ pages/exam/[id].tsx
@@ -1,8 +1,17 @@
 import { ChangeEvent, useEffect, useState } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import QuestionArea from "../components/QuestionArea";
 import { Question } from "@/models/Question";
 import styles from "@/styles/exam.module.css";
+import Image from "next/image";
+import menu from "@/public/menu.svg";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+import Modal from 'react-bootstrap/Modal';
+import { Button } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 interface QuestionResponse {
   questions: Question[];
@@ -13,6 +22,12 @@ export default function ExamPage () {
   const { id } = router.query;
   const [questionRes, setQuestionRes] = useState<QuestionResponse | null>(null)
   const [title, setTitle] = useState<string | null>(null);
+  
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   useEffect(() => {
     if (typeof id === 'string') {
@@ -39,10 +54,79 @@ export default function ExamPage () {
     router.push(`/exam/${currentExamId}`);
   }
 
+  const handleNewQuestion = () => {
+    handleShow();
+  }
+
   return (
     <div>
-      <h1 className={styles.examHeading}>{title && title}</h1>
+      <div className={styles.examHeading}>
+        {/*<button className={styles.menuButton}><Image src={menu} alt='menu-icon'></Image></button>*/}
+        <DropdownButton variant='light' title={<Image src={menu} alt='menu-icon'></Image>}>
+          <Dropdown.Item className={styles.menuItem} onClick={handleNewQuestion}>Create Question</Dropdown.Item>
+          <Dropdown.Item className={styles.menuItem}>Delete Exam</Dropdown.Item>
+        </DropdownButton>
+        <h1>{title && title}</h1>
+        <div className={styles.placeHolder}></div>
+      </div>
       <hr />
+
+      <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Question</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Question</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={2}
+                autoFocus
+              />
+            </Form.Group>
+            <br />
+            <hr />
+
+            <Form.Group>
+              <Form.Label>Choice A</Form.Label>
+              <Form.Control type="text" />
+
+              <Form.Label>Choice B</Form.Label>
+              <Form.Control type="text" />
+
+              <Form.Label>Choice C</Form.Label>
+              <Form.Control type="text" />
+
+              <Form.Label>Choice D</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <br />
+            <hr />
+            <Form.Label>Answer</Form.Label>
+            <Form.Group>
+              <Form.Check name="answer" required inline label="A" type='radio' />
+              <Form.Check name="answer" required inline label="B" type='radio' />
+              <Form.Check name="answer" required inline label="C" type='radio' />
+              <Form.Check name="answer" required inline label="D" type='radio' />
+            </Form.Group>
+            
+
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+
       {questionRes !== null && <QuestionArea questions={ questionRes.questions }></QuestionArea>}
     </div>
   );
